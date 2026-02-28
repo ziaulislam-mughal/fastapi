75139ded8cee918613c700cfc983d478ccfdb177
ziaulislam-mughal <ziaulislammughal0@gmail.com>
2026-03-01 00:52:58 +0500

    view specific patient data

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import json
 app = FastAPI()
 
 def load_data():
-    """
-    Description:
-
-    .
-    """
     #load data from json file 
     with open("patient.json" , "r") as f:
         data = json.load(f)
@@ -17,29 +12,26 @@ def load_data():
 
 @app.get("/")
 def hello():
-    """
-    Description:
-
-    .
-    """
     return {"message": "Patient Managment System API!"}
 
 @app.get("/about")
 def about():
-    """
-    Description:
-
-    .
-    """
     return {"message": "This is a simple API for managing patient records."}
 
 #view 
 @app.get("/view")
 def view_data():
-    """
-    Description:
-
-    .
-    """
     data = load_data()
     return data
+
+#view specific patient data 
+@app.get('/patient/{patient_id}')
+def view_patient_id(patient_id: str):
+    data = load_data()
+    patient = next(
+        (p for p in data if p["patient_id"] == patient_id),
+        None
+    )
+    if patient:
+        return patient
+    return {"error": "Patient not found."}
